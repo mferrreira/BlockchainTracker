e233e0cec2131b55e45c6ce6c2ace456d5a63d84
Márcio Martins Ferreira Júnior <masterplays02@gmail.com>
2025-03-15 21:24:30 -0300

    modelling redone

---
+++ telegram/bot.py
@@ -0,0 +1,9 @@
+import telebot
+
+bot = telebot.TeleBot('YOUR_TOKEN')
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Olá! Eu sou um bot.")
+
+bot.polling()
